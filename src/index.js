2ec2fbbbae1289417993e76a9f35e71792e037ce
deepaksaroha <deepaksaroha1993@gmail.com>
2021-11-23 15:36:21 +0530

    updated import statement for changed file name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppRouter from './components/router';
+import AppRouter from './components/Router';
 
 ReactDOM.render(
   <React.StrictMode>

6a10db276e68a173585e33ed039fa2d7f0cd0568
deepaksaroha <deepaksaroha1993@gmail.com>
2021-12-22 00:06:16 +0530

    changed DB from test to RRDB

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require( 'mongoose')
 
 exports.connectDB = _ =>{
     try{
-        return mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true})
+        return mongoose.connect('mongodb://localhost/RRDB', {useNewUrlParser: true, useUnifiedTopology: true})
     }catch(error){
         console.log('Error Connecting to DB', error);
     }

894cdb7eb57b9fb34e0a17ddfff331215dc3aa26
deepaksaroha <deepaksaroha1993@gmail.com>
2021-12-22 02:34:06 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-const api = require('./server/api/api')
-const db = require('./server/db/db')
-
-const genuuid = require('uuid').v4;
-const MongoStore = require('connect-mongo')
-const path = require('path');
-
+const express = require('express')
+const { connectDB, getClient } = require('./server/db/db')
 const session = require('express-session')
-const express  = require('express');
+const genuuid = require('uuid').v4;
+const MongoStore = require('connect-mongo');
+const path = require('path');
+const Book = require('./server/db/models/books')
 
 const app = express();
-
 const port = process.env.PORT || 4000;
+const api = require('./server/api/api')
 
+connectDB()
+.then(() => {
+    console.log('sada')
+    const Bookdata = new Book({ title: "Opening Places: An Anthology of Contemporary African Women's Writing", bookId: "5", author: "Yvonne Vera", edition: "1", descriptions: "In this anthology the award-winning author Yvonne Vera brings together the stories of many talented writers from different parts of Africa.", isbn: "9780435910106", languages: ["English", "African"], availability: { "amazon": true, "Flipkart": false, "kindle": false } });
+    Bookdata.save();
 
-db.connectDB()
-.then(()=>{
-
-    app.use('/api', express.json());
-
+    //Handle /api with the api middleware
     app.use('/api', session({
-        genid(){
-            return genuuid();
+        genid() {
+            return genuuid() // use UUIDs for session IDs
         },
-        store: new MongoStore({ client: db.getClient() }),
-        secret: 'qwertyuiop',
+        store: new MongoStore({ client: getClient() }),
+        secret: 'secret',
         resave: false,
-        saveUninitialized: true
+        saveUninitialized: true,
     }), api);
 
     app.use(express.static(path.join(__dirname, 'build')));
 
-    app.get('*', (req, res, next)=>{
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    })
+    //Return index.html for routes not handled by build folder
+    app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
 
-    app.listen(port, ()=>{
-        console.log(`Server listening at port: ${port}`);
-    })
-
+    //Start listening on port
+    app.listen(port, () => {
+        console.log(`Server listening on port: ${port}`);
+    });
 })
-

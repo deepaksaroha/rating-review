242f17196c208094299cdceb7f518230e46f1563
deepaksaroha <deepaksaroha1993@gmail.com>
2021-12-21 12:47:05 +0530

    updated model name from book to Book

--- server/db/models/books.js
+++ server/db/models/books.js
@@ -63,6 +63,6 @@ const bookSchema = new Schema({
     }
 });
 
-const book = mongoose.model('book', bookSchema);
+const Book = mongoose.model('Book', bookSchema);
 
-module.exports = book;
+module.exports = Book;

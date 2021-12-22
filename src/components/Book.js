45fb3505d11cdf59f124dc6ff5bab4271e1d3855
deepaksaroha <deepaksaroha1993@gmail.com>
2021-12-23 00:10:26 +0530

    fixed promise.all function call

--- src/components/Book.js
+++ src/components/Book.js
@@ -23,11 +23,11 @@ class Book extends React.Component{
         const promise2 = axios.get('/api/users');
         const promise3 = axios.get(`/api/reviews/${this.props.match.params.bookId}`);
 
-        Promise.all(promise1, promise3)
-        .then(([res1, res3])=>{
+        Promise.all([promise1, promise3])
+        .then((responses)=>{
             this.setState({
-                bookData: res1.bookData,
-                bookReviews: res3.reviewData
+                bookData: responses[0].data.bookData,
+                bookReviews: responses[1].data.reviewList
             })
             return promise2;
         })
@@ -103,59 +103,71 @@ class Book extends React.Component{
 
     render(){
         const book = this.state.bookData;
-
         return(
             <React.Fragment>
                 <Navbar loginStatus={this.state.isLoggedIn} handleLogout={this.handleLogout} />
                 <div className="book-outer-box">
-                    <div className="thumbnail-box">
-                        <img className="book-thumbnail" src={Book1} alt='bookImg' />
-                    </div>
-                    <div>
-                        <div className="book-detail-box">
-                            <div>
-                                <div>
-                                    {parseInt(book.avgRating)}
-                                </div>
-                                <div>
-                                    {book.ratingCount} Ratings
-                                </div>
-                                <div>
-                                    {book.reviewCount} Reviews
-                                </div>
-                            </div>
-                            <div className="about-book">
-                                <div>
-                                    <p id="book-title">{book.title}</p>&nbsp;
-                                    <p id="book-author">by-{book.author}</p>
-                                </div>
-                                <p id="book-desc">
-                                    {book.description}
-                                </p>
-                            </div>
-                            <div>
-                                <div className="detail-box">
-                                    <p>edition: {book.edition}</p>
-                                    <p>published: {book.published}</p>
-                                    <p>ISBN: {book.isbn}</p>
-                                </div>
-                            </div>
+                    {
+                        Object.keys(book).length !== 0 ?
+                        <div>
+                        <div className="thumbnail-box">
+                            <img className="book-thumbnail" src={Book1} alt='bookImg' />
                         </div>
-                        <div className="user-review">
-                            <div>
-                                {
-                                    this.state.isLoggedIn ?
-                                        book.userReview !== undefined ?
-                                            !this.state.isEdit ?
-                                                <div>
-                                                    <div className="rate-box">
-                                                        <Star loginStatus={this.state.isLoggedIn} rating={book.userReview.rating} changeRating={()=>{}}/>
-                                                    </div>
+                        <div>
+                            <div className="book-detail-box">
+                                <div>
+                                    <div>
+                                        {parseInt(book.avgRating)}
+                                    </div>
+                                    <div>
+                                        {book.ratingCount} Ratings
+                                    </div>
+                                    <div>
+                                        {book.reviewCount} Reviews
+                                    </div>
+                                </div>
+                                <div className="about-book">
+                                    <div>
+                                        <p id="book-title">{book.title}</p>&nbsp;
+                                        <p id="book-author">by-{book.author}</p>
+                                    </div>
+                                    <p id="book-desc">
+                                        {book.description}
+                                    </p>
+                                </div>
+                                <div>
+                                    <div className="detail-box">
+                                        <p>edition: {book.edition}</p>
+                                        <p>published: {book.published}</p>
+                                        <p>ISBN: {book.isbn}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="user-review">
+                                <div>
+                                    {
+                                        this.state.isLoggedIn ?
+                                            book.userReview !== undefined ?
+                                                !this.state.isEdit ?
                                                     <div>
-                                                        <div className="user-review-box">{book.userReview.review}</div>
+                                                        <div className="rate-box">
+                                                            <Star loginStatus={this.state.isLoggedIn} rating={book.userReview.rating} changeRating={()=>{}}/>
+                                                        </div>
+                                                        <div>
+                                                            <div className="user-review-box">{book.userReview.review}</div>
+                                                        </div>
+                                                        <button className="book-modify-btn" onClick={this.handleEdit}>Edit</button>
                                                     </div>
-                                                    <button className="book-modify-btn" onClick={this.handleEdit}>Edit</button>
-                                                </div>
+                                                    :
+                                                    <div>
+                                                        <div className="rate-box">
+                                                            <Star loginStatus={this.state.isLoggedIn} rating={this.state.newRating} changeRating={this.changeRating}/>
+                                                        </div>
+                                                        <div>
+                                                            <textarea className="user-review-box" name="newReview" value={this.state.newReview} onChange={this.handleChange} placeholder="Write your review here"></textarea>
+                                                        </div>
+                                                        <button className="book-modify-btn" onClick={this.handleSubmitReview}>Done</button><button className="book-modify-btn" onClick={this.handleEditCancel}>Cancel</button>
+                                                    </div>
                                                 :
                                                 <div>
                                                     <div className="rate-box">
@@ -164,48 +176,49 @@ class Book extends React.Component{
                                                     <div>
                                                         <textarea className="user-review-box" name="newReview" value={this.state.newReview} onChange={this.handleChange} placeholder="Write your review here"></textarea>
                                                     </div>
-                                                    <button className="book-modify-btn" onClick={this.handleSubmitReview}>Done</button><button className="book-modify-btn" onClick={this.handleEditCancel}>Cancel</button>
+                                                    <button className="book-modify-btn" onClick={this.handleSubmitReview}>Submit</button>
                                                 </div>
                                             :
                                             <div>
-                                                <div className="rate-box">
-                                                    <Star loginStatus={this.state.isLoggedIn} rating={this.state.newRating} changeRating={this.changeRating}/>
-                                                </div>
-                                                <div>
-                                                    <textarea className="user-review-box" name="newReview" value={this.state.newReview} onChange={this.handleChange} placeholder="Write your review here"></textarea>
-                                                </div>
-                                                <button className="book-modify-btn" onClick={this.handleSubmitReview}>Submit</button>
+                                                <a className="login-btn" href="/login" >Login to Review</a>
                                             </div>
-                                        :
-                                        <div>
-                                            <a className="login-btn" href="/login" >Login to Review</a>
-                                        </div>
-                                }
+                                    }
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="book-avail-options">
-                        <button id="x" disabled={'availability' in book && !book.availability.amazon}>Amazon</button>
-                        <button id="y" disabled={'availability' in book && !book.availability.flipkart}>Flikart</button>
-                        <button id="z" disabled={'availability' in book && !book.availability.kindle}>Kindle</button>                        
-                    </div>
-                    <div className="all-reviews"> 
-                        {
-                           
-                            this.state.bookReviews.map(review=>{
-                                if(book.userReview === undefined || review._id !== book.userReview._id)
+                        <div className="book-avail-options">
+                            <button id="x" disabled={'availability' in book && !book.availability.amazon}>Amazon</button>
+                            <button id="y" disabled={'availability' in book && !book.availability.flipkart}>Flikart</button>
+                            <button id="z" disabled={'availability' in book && !book.availability.kindle}>Kindle</button>                        
+                        </div>
+                        </div>
+                        :
+                        ''
+                    }
+                    
+                    {
+                        this.state.bookReviews.length !== 0 ?
+                            <div className="all-reviews"> 
                                 {
-                                    return <div key={review._id} className="book-review">
-                                        <p>{review.userName}</p>
-                                        <p>{review.rating}</p>
-                                        <p>{review.review}</p>
-                                    </div>
-                                }else{
-                                    return <div key={review._id}>''</div>
+                                
+                                    this.state.bookReviews.map(review=>{
+                                        if(book.userReview === undefined || review._id !== book.userReview._id)
+                                        {
+                                            return <div key={review._id} className="book-review">
+                                                <p>{review.userName}</p>
+                                                <p>{review.rating}</p>
+                                                <p>{review.review}</p>
+                                            </div>
+                                        }else{
+                                            return <div key={review._id}>''</div>
+                                        }
+                                    })
                                 }
-                            })
-                        }
-                    </div>
+                            </div>
+                            :
+                            ''
+                    }
+                    
                 </div>
             </React.Fragment>
         )

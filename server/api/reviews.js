f714c7bb97ed36ba72ed62422b64f20886db5657
deepaksaroha <deepaksaroha1993@gmail.com>
2021-12-24 17:03:39 +0530

    added logic to update rating/review count as per new review

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Review = require('../db/models/reviews')
+const Book = require('../db/models/books')
 
 const router = express.Router();
 
@@ -22,15 +23,29 @@ router.post('/', (req, res, next)=>{
     if(newReview === undefined){
         newReview = ''
     }
-
+    
     if(!newRating || !bookId || !userId){
         res.status(400).send({error: 'Information incomplete'});
         return;
     }
 
-    Review.updateOne( {bookId: bookId, userId: userId}, { rating: newRating, review: newReview, date: Date.now() }, {upsert: true} )
-    .then(()=>{
-        res.status(201).send({message: "Review Added"});
+    Promise.all([Review.findOneAndUpdate({ bookId: bookId, userId: userId}, { rating: newRating, review: newReview }, { upsert: true }), Book.findOne({ bookId: bookId })])
+    .then(results=>{
+        console.log(results);
+        const oldReview = results[0];
+        const book = results[1];
+        if(oldReview === null){
+            const newAvgRating = (book.avgRating*book.ratingCount + newRating)/(book.ratingCount + 1);
+            const newRatingCount = book.ratingCount+1;
+            const newReviewCount = book.reviewCount+( newReview ===''? 0 : 1 );
+            return Book.updateOne({ bookId: bookId }, { avgRating: newAvgRating, ratingCount: newRatingCount, reviewCount: newReviewCount })
+        }else if(oldReview.rating !== newRating){
+            const newAvgRating = (book.avgRating*book.ratingCount - oldReview.rating + newRating)/book.ratingCount;
+            return Book.updateOne({ bookId: bookId }, { avgRating: newAvgRating })
+        }
+    })
+    .then((result)=>{
+        res.status(200).send({message: 'Review Added'})
     })
     .catch(error=>{
         res.status(501).send({error:'Internal server error!'});

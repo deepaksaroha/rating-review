ca3ddc22c93be2ac28cb3b9eba5c3a49c6022be6
deepaksaroha <deepaksaroha1993@gmail.com>
2021-12-24 20:42:21 +0530

    added cloud db path

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require( 'mongoose')
 
 exports.connectDB = _ =>{
     try{
-        return mongoose.connect('mongodb://localhost/RRDB', {useNewUrlParser: true, useUnifiedTopology: true})
+        return mongoose.connect('mongodb+srv://admin:admin@cluster0.afyc4.mongodb.net/RRDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
     }catch(error){
         console.log('Error Connecting to DB', error);
     }

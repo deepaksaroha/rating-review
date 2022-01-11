4d756715e5d2a696e31f402f7dca580955ae31e9
deepaksaroha <deepaksaroha1993@gmail.com>
2022-01-11 18:11:10 +0530

    fixed footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Footer(){
     return (
         <React.Fragment>
+            <div style={{padding: '40px+1em'}}></div>
             <div style={{backgroundColor: 'rgb(36, 26, 26)', width:'100%', textAlign: 'center', position:'fixed', bottom: '0', padding:'20px', color: 'white'}} className="footer">
                 Developed by : Deepak, &nbsp;
                 {/* <span style={{color: 'red'}}>&#10084;</span> */}

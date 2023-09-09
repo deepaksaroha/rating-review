377fdffc591b89784ab5ce305f86c9c092900dd0
deepaksaroha <deepaksaroha1993@gmail.com>
2023-09-09 20:07:21 +0530

    updated db creds

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require( 'mongoose')
 
 exports.connectDB = _ =>{
     try{
-        return mongoose.connect('mongodb+srv://admin:admin@cluster0.afyc4.mongodb.net/RRDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+        return mongoose.connect('mongodb+srv://RateReview:YkrVp1joxuof0F4i@cluster0.afyc4.mongodb.net/RRDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
     }catch(error){
         console.log('Error Connecting to DB', error);
     }
